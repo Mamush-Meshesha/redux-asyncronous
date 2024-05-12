f020b9196ad732039af007255682b4ee027f5e81
Mamush-Meshesha <mamgithub@gmail.com>
2024-05-12 10:36:27 +0300

    actions

--- posts.js
+++ posts.js
@@ -1,5 +1,32 @@
+const { createStore } = require("redux")
+
 //initial state
+const initialState = {
+    posts: []
+}
 //actions
+const fetchPost = () => {
+    return {
+        type: 'FETCH_POST',
+    }
+}
+const fetchPostSuccess = () => {
+    return {
+        type: 'FETCH_POST_SUCCESS',
+    }
+}
+const fetchPostFailure = () => {
+    return {
+        type: "FETCH_POST_FAILURE"
+    }
+}
 //reducers
+
+const postReducer = (state = initialState, action) => {
+    switch (action.type) { 
+
+    }
+}
 //store
+const store = createStore(postReducer)
 //dispatch
